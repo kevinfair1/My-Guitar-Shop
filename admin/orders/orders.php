01f606d98280d11c0884183f1b9526c6e0dafebb
oberlt2922 <53795023+oberlt2922@users.noreply.github.com>
2020-11-16 13:46:59 -0500

    orers table
    display the orders in a table, allow the user to switch between shipped orders and outstanding orders

--- admin/orders/orders.php
+++ admin/orders/orders.php
@@ -1,49 +1,50 @@
 <?php include 'view/header.php'; ?>
 <?php include 'view/sidebar_admin.php'; ?>
+<style>
+    table {
+    border: 1px solid #001963;
+    border-collapse: collapse;
+    }
+    td, th {
+        border: 1px dashed #001963;
+        padding: .2em .5em .2em .5em;
+        vertical-align: top;
+        text-align: left;
+    }
+</style>
 <main>
+    <?php if($action == 'view_new_orders'): ?>
     <h1>Outstanding Orders</h1>
-    <?php if (count($new_orders) > 0 ) : ?>
-        <ul>
-            <?php foreach($new_orders as $order) :
-                $order_id = $order['orderID'];
-                $order_date = strtotime($order['orderDate']);
-                $order_date = date('M j, Y', $order_date);
-                $url = $app_path . 'admin/orders' .
-                       '?action=view_order&amp;order_id=' . $order_id;
-                ?>
-                <li>
-                    <a href="<?php echo $url; ?>">Order # 
-                    <?php echo $order_id; ?></a> for
-                    <?php echo $order['firstName'] . ' ' .
-                               $order['lastName']; ?> placed on
-                    <?php echo $order_date; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <p>There are no shipped orders.</p>
+    <a href="index.php?action=view_old_orders">View Shipped Orders</a>
     <?php endif; ?>
+    <?php if($action == 'view_old_orders'): ?>
     <h1>Shipped Orders</h1>
-    <?php if (count($old_orders) > 0 ) : ?>
-        <ul>
-            <?php foreach($old_orders as $order) :
-                $order_id = $order['orderID'];
-                $order_date = strtotime($order['orderDate']);
-                $order_date = date('M j, Y', $order_date);
-                $url = $app_path . 'admin/orders' .
-                       '?action=view_order&amp;order_id=' . $order_id;
-                ?>
-                <li>
-                    <a href="<?php echo $url; ?>">Order #
-                    <?php echo $order_id; ?></a> for
-                    <?php echo htmlspecialchars($order['firstName']) . ' ' .
-                               htmlspecialchars($order['lastName']); ?> placed on
-                    <?php echo $order_date; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+    <a href="index.php?action=view_new_orders">View Outstanding Orders</a>
+    <?php endif; ?>
+    <?php if (count($orders) > 0 ) : ?>
+    <table>
+        <tr>
+            <th>Order Number</th>
+            <th>Customer</th>
+            <th>Order Date</th>
+            <th>&nbsp;</th>
+        </tr>
+        <?php foreach($orders as $order): 
+            $order_id = $order['orderID'];
+            $order_date = strtotime($order['orderDate']);
+            $order_date = date('M j, Y', $order_date);
+            $url = $app_path . 'admin/orders' . '?action=view_order&amp;order_id=' . $order_id;
+        ?>
+        <tr>
+            <td><?php echo $order_id; ?></td>
+            <td><?php echo htmlspecialchars($order['firstName']) . ' ' . htmlspecialchars($order['lastName']); ?></td>
+            <td><?php echo $order_date; ?></td>
+            <td><a href='<?php echo $url; ?>'>View</a></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
     <?php else: ?>
-        <p>There are no shipped orders.</p>
+        <p>There are no orders.</p>
     <?php endif; ?>
 </main>
 <?php include 'view/footer.php'; ?>
